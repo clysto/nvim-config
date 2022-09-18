de598b189c43ae6855b4998dcfbf34c178153ed0
毛亚琛 <maoyachen55@gmail.com>
2022-09-18 16:47:17 +0800

    change keybinding

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -38,7 +38,12 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
-				cmp.select_next_item()
+				local entry = cmp.get_selected_entry()
+				if not entry then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					cmp.confirm()
+				end
 			else
 				fallback()
 			end
